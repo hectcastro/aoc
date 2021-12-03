9337d27cefa2e24f9312e60a3125fec430c1c5d2
Hector Castro <hectcastro@gmail.com>
2021-12-02 20:43:55 -0500

    feat: add first solution for 2021/2

---
+++ 2021/2/1.py
@@ -0,0 +1,27 @@
+from collections import namedtuple
+import fileinput
+from fileinput import FileInput
+
+Command = namedtuple("Command", ["direction", "unit"])
+
+
+def handler(raw_commands: FileInput) -> int:
+    commands = [
+        Command(*[instruction.strip() for instruction in raw_command.split(" ")])
+        for raw_command in raw_commands
+    ]
+    horizontal_position, depth = 0, 0
+
+    for command in commands:
+        if command.direction == "forward":
+            horizontal_position += int(command.unit)
+        elif command.direction == "down":
+            depth -= int(command.unit)
+        elif command.direction == "up":
+            depth += int(command.unit)
+
+    return abs(horizontal_position) * abs(depth)
+
+
+if __name__ == "__main__":
+    print(handler(fileinput.input()))
